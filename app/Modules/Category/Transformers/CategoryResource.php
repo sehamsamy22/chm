2e85581422cbee1d16e2e0f385247a5b0e9dd8d4
seham samy <seham@ejjadh.com>
2022-03-01 17:07:44 +0200

    productResource

--- app/Modules/Category/Transformers/CategoryResource.php
+++ app/Modules/Category/Transformers/CategoryResource.php
@@ -16,22 +16,24 @@ class CategoryResource extends JsonResource
      */
     public function toArray($request)
     {
+        dd($this->packageItems());
         return [
             'id' => $this->id,
-            'name' => $request->header('Content-language') ? $this->name : $this->getTranslations('name')??'',
+            'name' => $request->header('Content-language') ? $this->name : $this->getTranslations('name') ?? '',
             'image' => $this->image,
             'have_additions' => $this->have_additions,
             'parentCategory' => $this->mainCategory,
+            'is_package' => $this->is_package,
             "subcategories" => $this->subcategories->map(function ($subcategory) use ($request) {
                 return [
                     "id" => $subcategory->id,
-                    "name" => $request->header('content-language') ? $subcategory->name : $subcategory->getTranslations('name')??'',
+                    "name" => $request->header('content-language') ? $subcategory->name : $subcategory->getTranslations('name') ?? '',
                     "image" => $subcategory->image,
                     "options" => CategoryOptionResource::collection($subcategory->categoryOptions),
                 ];
             }),
             "options" => CategoryOptionResource::collection($this->categoryOptions),
-            "products" => ProductResource::collection($this->products),
+            "products" => ProductResource::collection($this->packageItems()),
         ];
     }
 }
