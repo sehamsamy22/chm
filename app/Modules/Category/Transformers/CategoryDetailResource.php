891060d87c820886d1600617f2e079170f6dedcd
seham samy <seham@ejjadh.com>
2022-03-13 16:44:44 +0200

    mostorderdProduct

--- app/Modules/Category/Transformers/CategoryDetailResource.php
+++ app/Modules/Category/Transformers/CategoryDetailResource.php
@@ -35,7 +35,7 @@ class CategoryDetailResource extends JsonResource
 //            "products" => ProductResource::collection($this->products),
             "moreOrderedProduct" => $this->products()->with('orders')->get()->sortByDesc(function ($product) {
                 return $product->orders->count();
-            })->take(3),
+            })->take(3)->flatten(),
             "maxProductPrice" => (!$products->isEmpty()) ? $products->sortBy([['price', 'desc']])->first()->price : 0,
             "minProductPrice" => (!$products->isEmpty()) ? $products->sortBy([['price', 'asc']])->first()->price : 0,
         ];
