afd54966b1fc9264b051286c7591347b20a21f8e
seham samy <seham@ejjadh.com>
2022-06-26 16:13:37 +0200

    add service product to cart

---
+++ app/Modules/Cart/ValidationRules/ProductTypeService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Modules\Cart\ValidationRules;
+
+use App\Modules\Product\Entities\Product;
+use App\Scopes\NormalProductScope;
+use App\Services\Validation\RulesInterface;
+use App\Services\Validation\ValidationError;
+use Illuminate\Support\Facades\Auth;
+
+class ProductTypeService implements RulesInterface
+{
+    private $item;
+
+    public function __construct($item)
+    {
+        $this->item = $item;
+    }
+
+    public function validate()
+    {
+        $cart = Auth::user()->cart;
+        $product =Product::withoutGlobalScope(NormalProductScope::class)->find($this->item['product_id']);
+        if ($product->type=='service' && count($cart->items)!=0){
+            return new ValidationError("sorry,please empty your cart first", 423);
+        }
+        if ($product->type!='service' && count($cart->items)!=0){
+            foreach ($cart->items as $item){
+                if($item->type=='service') return new ValidationError("sorry,you cart have service product checkout before", 423);
+            }
+        }
+    }
+}
